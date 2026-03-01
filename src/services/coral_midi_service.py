c90d19edb28c2f55823f0353f2f7a7e6447f30e7
AkaruiKyuketsuki <ddallaco@ucm.es>
2026-03-01 13:53:34 +0100

    Hecha la vuelta atras desde las ventanas y creados los .py de los nuevos servicios

---
+++ src/services/coral_midi_service.py
@@ -0,0 +1,4 @@
+"""
+Servicio encargado de generar archivos MIDI
+a partir de partes individuales.
+"""
---
+++ src/services/coral_parser_service.py
@@ -0,0 +1,4 @@
+"""
+Servicio encargado de analizar el MusicXML
+y extraer información estructural coral.
+"""
