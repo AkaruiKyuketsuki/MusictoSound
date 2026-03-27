b1a099d2d2be096013d1a0963f51a3d2fe62664e
AkaruiKyuketsuki <ddallaco@ucm.es>
2026-03-27 14:38:51 +0100

    añadidas las pestañas tipo notebook para ver varias letras

--- src/views/lyrics_editor_view.py
+++ src/views/lyrics_editor_view.py
@@ -28,6 +28,7 @@ def open_lyrics_editor(parent, xml_path):
     # Frame scrollable
     # ===============================
     
+    """
     canvas = tk.Canvas(main)
     scrollbar = ttk.Scrollbar(main, orient="vertical", command=canvas.yview)
 
@@ -44,12 +45,21 @@ def open_lyrics_editor(parent, xml_path):
     canvas.pack(side="left", fill="both", expand=True)
     scrollbar.pack(side="right", fill="y")
 
+    """
+    # ===============================
+    # Pestañas de voces
+    # ===============================
+
+    notebook = ttk.Notebook(main)
+    notebook.pack(fill="both", expand=True)
+
     # ===============================
     # Cargar sílabas
     # ===============================
 
     syllables_by_part = extract_syllables_by_part(xml_path)
 
+    """
     for part_name, syllables in syllables_by_part.items():
 
         part_frame = ttk.LabelFrame(scroll_frame, text=part_name, padding=10)
@@ -75,7 +85,56 @@ def open_lyrics_editor(parent, xml_path):
             )
 
             lbl.pack(side="left", padx=2)
-    
+    """
+
+    for part_name, syllables in syllables_by_part.items():
+
+        # Crear pestaña
+        tab = ttk.Frame(notebook)
+        notebook.add(tab, text=part_name)
+
+        canvas = tk.Canvas(tab)
+        scrollbar = ttk.Scrollbar(tab, orient="vertical", command=canvas.yview)
+
+        scroll_frame = ttk.Frame(canvas)
+
+        scroll_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+        )
+
+        canvas.create_window((0,0), window=scroll_frame, anchor="nw")
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        canvas.pack(side="left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
+
+        # ===============================
+        # Frame de sílabas
+        # ===============================
+
+        part_frame = ttk.Frame(scroll_frame, padding=10)
+        part_frame.pack(fill="x", pady=10)
+
+        syllables_per_row = 12
+        row = None
+
+        for i, syl in enumerate(syllables):
+
+            if i % syllables_per_row == 0:
+                row = ttk.Frame(part_frame)
+                row.pack(anchor="w", pady=2)
+
+            lbl = ttk.Label(
+                row,
+                text=syl,
+                relief="solid",
+                padding=5
+            )
+
+            lbl.pack(side="left", padx=2)
+            
+        
     # ===============================
     # Botones inferiores
     # ===============================
